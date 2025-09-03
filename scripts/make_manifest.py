d8a0841d4a4d0164cf2b0d4a2268f42afb7d16b7
Leon van Wissen <l.vanwissen@uva.nl>
2025-09-03 17:37:31 +0200

    update model and data

--- scripts/make_manifest.py
+++ scripts/make_manifest.py
@@ -9,8 +9,7 @@ iiif_prezi3.load_bundled_extensions()
 URI_PREFIX = "https://amsterdamtimemachine.github.io/amsterdam-in-motion-750/iiif/"
 
 
-def main(df_protest, df_photo, target_folder="iiif"):
-
+def main(df_protest, df_photo, df_classification, target_folder="iiif"):
     """
     Generates IIIF manifests from protest and photo data.
 
@@ -18,21 +17,29 @@ def main(df_protest, df_photo, target_folder="iiif"):
     slug	naam	beschrijving	datum_start	datum_eind	locatie's	classificatie's	thumbnail (foto op homepage)	wikidata	extra informatie
 
     Data structure df_photo:
-    index	uri	protest	naam	beschrijving	datum	fotograaf	url	locatie	iiif_info_json
+    index	uri	protest	naam	beschrijving	datum_start	datum_eind	fotograaf	archief	url	locatie	iiif_canvas_id	iiif_info_json
 
     Args:
         df_protest (pd.DataFrame): DataFrame containing protest data.
         df_photo (pd.DataFrame): DataFrame containing photo data.
     """
 
+    classification_label2concept = dict()
+    for _, row in df_classification.iterrows():
+        classification_label2concept[row["prefLabel"]] = {
+            "@id": row["uri"],
+            "@type": "skos:Concept",
+            "skos:prefLabel": {"@language": "nl", "@value": row["prefLabel"]},
+        }
+
     collection = iiif_prezi3.Collection(
         id=URI_PREFIX + "collection.json",
         label="Protest - Amsterdam in Motion 750",
         summary="Overzicht van protestfoto's uit Amsterdam.",
-        items=[]
+        items=[],
     )
 
-    for index, protest_row in df_protest.iterrows():
+    for _, protest_row in df_protest.iterrows():
 
         slug = protest_row["slug"]
         manifest_uri = URI_PREFIX + slug + ".json"
@@ -56,10 +63,47 @@ def main(df_protest, df_photo, target_folder="iiif"):
                 )
                 nav_date = ts.isoformat()
 
+        protest_sdo = {
+            "@type": "schema:Event",
+            "schema:name": protest_row["naam"] if pd.notna(protest_row["naam"]) else "",
+            "schema:description": (
+                protest_row["beschrijving"]
+                if pd.notna(protest_row["beschrijving"])
+                else ""
+            ),
+            "schema:startDate": {
+                "@type": "xsd:date",
+                "@value": protest_row["datum_start"][:10],
+            },
+            "schema:endDate": {
+                "@type": "xsd:date",
+                "@value": protest_row["datum_eind"][:10],
+            },
+            "schema:location": [
+                {
+                    "@type": "schema:Place",
+                    "schema:name": (
+                        protest_row["locatie's"]
+                        if pd.notna(protest_row["locatie's"])
+                        else ""
+                    ),
+                }
+            ],
+            "schema:additionalType": [
+                classification_label2concept.get(i.strip())
+                for i in protest_row["classificatie's"].split(", ")
+                if i in classification_label2concept
+            ],
+        }
+
         manifest = iiif_prezi3.Manifest(
             id=manifest_uri,
-            label=protest_row["naam"],
-            summary=protest_row["beschrijving"],
+            label=protest_row["naam"] if pd.notna(protest_row["naam"]) else "",
+            summary=(
+                protest_row["beschrijving"]
+                if pd.notna(protest_row["beschrijving"])
+                else ""
+            ),
             items=[],
             navDate=nav_date,
             metadata=[
@@ -69,9 +113,11 @@ def main(df_protest, df_photo, target_folder="iiif"):
                     label={"nl": ["Datum (begin)"]},
                     value={
                         "nl": [
-                            str(protest_row["datum_start"])
-                            if pd.notna(protest_row["datum_start"])
-                            else ""
+                            (
+                                str(protest_row["datum_start"][:10])
+                                if pd.notna(protest_row["datum_start"])
+                                else ""
+                            )
                         ]
                     },
                 ),
@@ -79,9 +125,11 @@ def main(df_protest, df_photo, target_folder="iiif"):
                     label={"nl": ["Datum (eind)"]},
                     value={
                         "nl": [
-                            str(protest_row["datum_eind"])
-                            if pd.notna(protest_row["datum_eind"])
-                            else ""
+                            (
+                                str(protest_row["datum_eind"][:10])
+                                if pd.notna(protest_row["datum_eind"])
+                                else ""
+                            )
                         ]
                     },
                 ),
@@ -89,9 +137,11 @@ def main(df_protest, df_photo, target_folder="iiif"):
                     label={"nl": ["Locatie's"]},
                     value={
                         "nl": [
-                            protest_row["locatie's"]
-                            if pd.notna(protest_row["locatie's"])
-                            else ""
+                            (
+                                protest_row["locatie's"]
+                                if pd.notna(protest_row["locatie's"])
+                                else ""
+                            )
                         ]
                     },
                 ),
@@ -99,9 +149,25 @@ def main(df_protest, df_photo, target_folder="iiif"):
                     label={"nl": ["Classificatie's"]},
                     value={
                         "nl": [
-                            protest_row["classificatie's"]
-                            if pd.notna(protest_row["classificatie's"])
-                            else ""
+                            (
+                                protest_row["classificatie's"]
+                                if pd.notna(protest_row["classificatie's"])
+                                else ""
+                            )
+                        ]
+                    },
+                ),
+                iiif_prezi3.KeyValueString(
+                    label={"nl": ["Classificatie's (URI)"]},
+                    value={
+                        "nl": [
+                            ", ".join(
+                                [
+                                    classification_label2concept[i.strip()]["@id"]
+                                    for i in protest_row["classificatie's"].split(", ")
+                                    if i in classification_label2concept
+                                ]
+                            )
                         ]
                     },
                 ),
@@ -113,6 +179,9 @@ def main(df_protest, df_photo, target_folder="iiif"):
             df_photo["protest"] == protest_row["naam"]
         ].iterrows():
 
+            if pd.isna(photo_row["iiif_info_json"]):
+                continue
+
             canvas_id = f"{URI_PREFIX}{slug}/p1/canvas/{i+1}"
 
             manifest.make_canvas_from_iiif(
@@ -120,6 +189,97 @@ def main(df_protest, df_photo, target_folder="iiif"):
                 id=canvas_id,
                 anno_page_id=canvas_id + "/page",
                 anno_id=canvas_id + "/anno",
+                label=photo_row["naam"] if pd.notna(photo_row["naam"]) else "",
+                metadata=[
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Naam"]},
+                        value={
+                            "nl": [
+                                photo_row["naam"] if pd.notna(photo_row["naam"]) else ""
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Beschrijving"]},
+                        value={
+                            "nl": [
+                                (
+                                    photo_row["beschrijving"]
+                                    if pd.notna(photo_row["beschrijving"])
+                                    else ""
+                                )
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Datum (begin)"]},
+                        value={
+                            "nl": [
+                                (
+                                    str(photo_row["datum_start"][:10])
+                                    if pd.notna(photo_row["datum_start"])
+                                    else ""
+                                )
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Datum (eind)"]},
+                        value={
+                            "nl": [
+                                (
+                                    str(photo_row["datum_eind"][:10])
+                                    if pd.notna(photo_row["datum_eind"])
+                                    else ""
+                                )
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Fotograaf"]},
+                        value={
+                            "nl": [
+                                (
+                                    photo_row["fotograaf"]
+                                    if pd.notna(photo_row["fotograaf"])
+                                    else ""
+                                )
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Archief"]},
+                        value={
+                            "nl": [
+                                (
+                                    photo_row["archief"]
+                                    if pd.notna(photo_row["archief"])
+                                    else ""
+                                )
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["URL"]},
+                        value={
+                            "nl": [
+                                photo_row["url"] if pd.notna(photo_row["url"]) else ""
+                            ]
+                        },
+                    ),
+                    iiif_prezi3.KeyValueString(
+                        label={"nl": ["Locatie"]},
+                        value={
+                            "nl": [
+                                (
+                                    photo_row["locatie"]
+                                    if pd.notna(photo_row["locatie"])
+                                    else ""
+                                )
+                            ]
+                        },
+                    ),
+                ],
             )
 
         with open(f"{target_folder}/{slug}.json", "w", encoding="utf-8") as f:
@@ -128,9 +288,10 @@ def main(df_protest, df_photo, target_folder="iiif"):
                 "http://iiif.io/api/extension/navplace/context.json",
                 "http://iiif.io/api/presentation/3/context.json",
             ]
+            manifest_jsonld["seeAlso"] = [protest_sdo]
             json.dump(manifest_jsonld, f, indent=2, ensure_ascii=False)
 
-        collection.items.append(manifest)
+        collection.add_item(manifest)
 
     # Save the collection as a JSON file
     with open(f"{target_folder}/collection.json", "w", encoding="utf-8") as f:
@@ -145,8 +306,20 @@ if __name__ == "__main__":
     df_protest = pd.read_excel(
         "data/Amsterdam in Motion 750 - Data.xlsx", sheet_name="Protest"
     )
+
+    # parse datum_start and datum_eind as string
+    df_protest["datum_start"] = df_protest["datum_start"].astype(str)
+    df_protest["datum_eind"] = df_protest["datum_eind"].astype(str)
+
     df_photo = pd.read_excel(
         "data/Amsterdam in Motion 750 - Data.xlsx", sheet_name="Foto"
     )
 
-    main(df_protest, df_photo)
+    df_photo["datum_start"] = df_photo["datum_start"].astype(str)
+    df_photo["datum_eind"] = df_photo["datum_eind"].astype(str)
+
+    df_classification = pd.read_excel(
+        "data/Amsterdam in Motion 750 - Data.xlsx", sheet_name="Classificatie"
+    )
+
+    main(df_protest, df_photo, df_classification)
