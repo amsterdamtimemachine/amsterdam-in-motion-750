59e6e8a4d44a2e4848eb2f3789885f018f555e7e
Leon van Wissen <l.vanwissen@uva.nl>
2025-09-09 20:19:05 +0200

    update data

--- scripts/make_manifest.py
+++ scripts/make_manifest.py
@@ -314,7 +314,7 @@ if __name__ == "__main__":
     df_protest["classificatie's"] = df_protest["classificatie's"].astype(str)
 
     df_photo = pd.read_excel(
-        "data/Amsterdam in Motion 750 - Data.xlsx", sheet_name="Foto"
+        "data/Amsterdam in Motion 750 - Data.xlsx", sheet_name="Foto_kopie"
     )
 
     df_photo["datum_start"] = df_photo["datum_start"].astype(str)
